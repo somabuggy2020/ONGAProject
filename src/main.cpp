84d2ba95559ff5a4354e72b437ba4c5e23e38e0b
Ayumu <tommy.tommy.walker@gmail.com>
2020-10-04 00:07:52 +0900

    save

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,13 @@ int main(int argc, char *argv[])
 
 	QApplication a(argc, argv);
 	MainWindow w;
+	w.setWindowState(Qt::WindowState::WindowMaximized);
+
 //	QDesktopWidget dw;
 //	QRect mainScreen = dw.availableGeometry(dw.primaryScreen());
 //	w.resize(mainScreen.width()*0.5,mainScreen.height());
 //	w.move(mainScreen.width()*0.5,0);
 	w.show();
+
 	return a.exec();
 }
